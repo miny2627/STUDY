622c2f07a985b9eba349e2755d925ac7b48fa7d6
MinyoungChoi <miny2627@naver.com>
2021-01-14 15:56:52 +0900

    Create 10844_Easy_Num.cpp

---
+++ DP/10844_Easy_Num.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#define MOD 1000000000
+
+using namespace std;
+
+int main() {
+	int n, dp[101][10] = {};
+	int sum = 0;
+	cin >> n;
+	
+	for (int i = 0; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j == 0) {
+				dp[i][0] = dp[i - 1][1];
+			}
+			else if (j == 9) {
+				dp[i][9] = dp[i - 1][8];
+			}
+			else {
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+			}
+		}
+	}
+	for (int i = 1; i < 10; i++) {
+		sum = (sum + dp[n][i]) % MOD;
+	}
+	cout << sum % MOD;
+
+	return 0;
+}
